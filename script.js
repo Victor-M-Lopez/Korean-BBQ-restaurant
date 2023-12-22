2f371c4769c8f3378308356879afd22a026e1b17
Victor Lopez <vic.m.lopez114@gmail.com>
2023-12-21 19:12:49 -0500

    added function to beef and pork button so when display the other is not displayed

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function navScroll() {
     navBar.classList.remove('nav_blur');
   }
 }
+
+// let meatBtn = document.querySelector('.type-pork--btn');
+// let beefBtn = document.querySelector('.type-beef--btn');
+// let chickenBtn = document.querySelector('.type-chicken--btn');
+// let seaFoodBtn = document.querySelector('.type-seafood--btn');
+
+const porkContainer = document.querySelector('.pork_meat--container');
+const beefContainer = document.querySelector('.beef_meat--container');
+
+
+
+function displayPork() {
+  porkContainer.style.display = 'flex';
+  beefContainer.style.display = 'none';
+}
+
+function displayBeef() {
+  porkContainer.style.display = 'none';
+  beefContainer.style.display = 'flex';
+  beefContainer.style.opacity = '1';
+  
+}
