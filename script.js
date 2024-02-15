4476f8080046624918f2788af83a15f72f15f50f
Victor Lopez <vic.m.lopez114@gmail.com>
2024-02-15 11:34:31 -0500

    deleted some code

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 window.onscroll = function() {navScroll()};
 
 let navBar = document.getElementById('landing_page--nav');
+let card = document.querySelector('.meats__link');
 
 function navScroll() {
 
@@ -13,75 +14,12 @@ function navScroll() {
   }
 }
 
-let porkBtn = document.querySelector('.type-pork--btn');
-let beefBtn = document.querySelector('.type-beef--btn');
-let chickenBtn = document.querySelector('.type-chicken--btn');
-let seaFoodBtn = document.querySelector('.type-seafood--btn');
 
-const porkContainer = document.querySelector('.pork_meat--container');
-const beefContainer = document.querySelector('.beef_meat--container');
-const chickenContainer = document.querySelector('.chicken_meat--container');
-const seaFoodContainer = document.querySelector('.seafood_meat--container');
-
-
-
-function displayPork() {
-  porkContainer.style.display = 'flex';
-  beefContainer.style.display = 'none';
-  chickenContainer.style.display = 'none';
-  seaFoodContainer.style.display = 'none';
-  
-  porkBtn.classList.add('button-hover--effect');
-  beefBtn.classList.remove('button-hover--effect');
-  chickenBtn.classList.remove('button-hover--effect');
-  seaFoodBtn.classList.remove('button-hover--effect');
+function flipCard() {
+  card.style.transform = 'rotateY(180deg)';
+  card.style.transition = 'transform 800ms ease';
 }
 
-function displayBeef() {
-  beefContainer.style.display = 'flex';
-  porkContainer.style.display = 'none';
-  chickenContainer.style.display = 'none';
-  seaFoodContainer.style.display = 'none';
-  
-  beefBtn.classList.add('button-hover--effect');
-  porkBtn.classList.remove('button-hover--effect');
-  seaFoodBtn.classList.remove('button-hover--effect');
-  chickenBtn.classList.remove('button-hover--effect');
-  
-}
-
-function displayChicken() {
-  chickenContainer.style.display = "flex";
-  porkContainer.style.display = 'none';
-  beefContainer.style.display = 'none'; 
-  seaFoodContainer.style.display = 'none';
-  
-  chickenBtn.classList.add('button-hover--effect');
-  porkBtn.classList.remove('button-hover--effect');
-  seaFoodBtn.classList.remove('button-hover--effect');
-  beefBtn.classList.remove('button-hover--effect');
-}
-
-function displaySeafood() {
-  seaFoodContainer.style.display = 'flex';
-  chickenContainer.style.display = "none";
-  porkContainer.style.display = 'none';
-  beefContainer.style.display = 'none'; 
-  
-  seaFoodBtn.classList.add('button-hover--effect');
-  porkBtn.classList.remove('button-hover--effect');
-  chickenBtn.classList.remove('button-hover--effect');
-  beefBtn.classList.remove('button-hover--effect');
-}
-
-
-let meatImage = document.querySelector('.meat--box');
-let bodyBackground = document.getElementById('meat_page--body');
-
-function moreInfo() {
-  meatImage.classList.add('more_info--effect');
-  bodyBackground.style.filter = 'brightness(0.8)'
-}
 
 function openMenu() {
   document.body.classList += " menu--open" 
