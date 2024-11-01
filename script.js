a9a85245435b0ece3b29aee0e62ebc0c242fb4d8
Victor Lopez <vic.m.lopez114@gmail.com>
2024-11-01 14:08:12 -0400

    added modal on nav

--- script.js
+++ script.js
@@ -3,13 +3,40 @@ window.onscroll = function() {navScroll()};
 let navBar = document.querySelector('nav');
 
 function navScroll() {
+  let navWhiteElements = navBar.querySelectorAll('.nav_white');
+  let menuText = navBar.querySelector('.menu_link');
+  
+  for( let i = 0; i < navWhiteElements.length; i++) {
+    if (document.body.scrollTo > 150 || document.documentElement.scrollTop > 150) {
+      navBar.classList.add('nav_change');
+      navWhiteElements[i].classList.add('color_change');
+      menuText.style.backgroundColor = '#fff';
+    }
+    else {
+      navBar.classList.remove('nav_change');
+      navWhiteElements[i].classList.remove('color_change');
+      menuText.style.backgroundColor = '#E5D679';
+    }
 
-  if (document.body.scrollTo > 150 || document.documentElement.scrollTop > 150) {
-    navBar.classList.add('nav_change');
   }
   
-  else {
-    navBar.classList.remove('nav_change');
-  }
 }
 
+
+let navBtn = document.getElementById('hamburger_menu');
+let navModalList = document.querySelector('.nav_modal-list');
+
+navBtn.addEventListener('click', function() {
+  let navBarElement = navBar.querySelectorAll('.nav_white');
+  navBar.classList.toggle('nav_view-change');
+  navModalList.classList.toggle('text_appear');
+
+  for( let i = 0; i < navBarElement.length; i++) {
+    if (document.body.scrollTo > 150 || document.documentElement.scrollTop > 150) {
+      navBarElement[i].classList.add('color_change');
+    }   
+    else {
+      navBarElement[i].classList.toggle('color_change');
+    }
+  }
+});
